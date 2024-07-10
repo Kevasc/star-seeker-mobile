3af05f16953da7696908ce54ef635f3fb287cb67
kevas cole <colekevas@gmail.com>
2024-07-10 11:21:47 +0100

    inserted background image

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+const PlaceholderImage = require("./assets/background-image.jpg");
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
       <Text style={{ color: "#fff", fontSize: "16px" }}>
         Welcome to Starseeker
       </Text>
