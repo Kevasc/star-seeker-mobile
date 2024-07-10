a65270bbe827dcaee47865b22c84e61b7e9c42ae
kevas cole <colekevas@gmail.com>
2024-07-10 14:57:02 +0100

    changed variable name

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
 
-const PlaceholderImage = require("./assets/background-image.jpg");
+const backgroundImage = require("./assets/background-image.jpg");
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <Image source={backgroundImage} style={styles.image} />
       </View>
       <Text style={styles.text}>Welcome to Starseeker</Text>
       <StatusBar style="auto" />
